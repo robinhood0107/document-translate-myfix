f6bbc73dd86f0b2b45ec52cf66d6300e60da6973
robinhood0107 <pjj4678@gmail.com>
2026-04-05 04:33:28 +0900

    fix(ci): avoid eager utility imports

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -1,3 +1,7 @@
-from .textblock import *
+"""Utility package namespace.
 
+Keep this package initializer intentionally light. Importing specific helper
+modules such as ``modules.utils.paths`` should not pull in heavyweight OCR or
+detection dependencies as a side effect.
+"""
 
